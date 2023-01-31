e73bae12bf8f692b770d3c03a72f2be8df02b6ed
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-01-31 01:01:22 +0100

    finissssh

--- Snack5/main5.js
+++ Snack5/main5.js
@@ -4,22 +4,25 @@
 //Alla fine stampa il numero di tentativi che ci sono voluti per indovinare il numero generato casualmente. (edited)
 
 const random_number = Math.floor(Math.random() * 100) + 1;
+console.log(random_number);
 
 let user_number = 0;
 
 let count = 0;
 
-while (random_number = user_number) {
+while (random_number != user_number) {
 
     user_number = parseInt(prompt("Indovina il numero, è compreso tra 0 e 100"));
 
     if (user_number < random_number) {
         alert("Il numero segreto è più grande di " + user_number);
-    } else {
+    } else if (user_number > random_number) {
         alert("il numero segreto è più piccolo di " + user_number);
+    } else {
+        alert("Sei proprio talentuoso , il numero era proprio " + user_number);
     }
 
     count++;
 }
 
-document.getElementById("stamp").append("Hai indovinato in " + count + " tentativi.");
+document.getElementById("stamp").append("Hai indovinato in " + count + " tentativi. Il numero era " + random_number + "!");
